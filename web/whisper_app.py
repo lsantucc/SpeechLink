cfcedcdd759e6162ffeae8d8f2b5850ac29b7b55
lgtipton4 <lgtipton4@gmail.com>
2025-04-09 16:22:05 -0400

    Updated text formatting

--- web/whisper_app.py
+++ web/whisper_app.py
@@ -1,14 +1,18 @@
 # Transcription/translation using whisper
 # Whisper can only take 30 seconds of audio at a time. If the input file is longer than 30 seconds
 #    we need to break it into chunks
+# If we return a lot of text it needs to be formatted to be displayed properly otherwise it will be one huge line
+# Need to improve speed by forcing GPU use and parallel processing
 
 # pip install -U openai-whisper py-chocolatey 
 # choco install ffmpeg (with elevated priveleges)
 import whisper
 import os
 from pydub import AudioSegment
+import shutil
 
 file_types = [".mp3", ".wav", ".m4a", ".flac", ".ogg", ".aac", ".wma", ".mp4", ".webm"]
+temp_dir = "temp"
 
 def remove_extension(string):
     for file_type in file_types:
@@ -28,23 +32,26 @@ def transcribe(filepath):
 
     # Medium seems to be the best balance of speed and accuracy
     model = whisper.load_model("medium")
-
+    device = "cuda" 
+    model = model.to(device)
+    filename, file_extension = os.path.splitext(filepath)
     # need to split audio into chunks of 30s
+    audio = AudioSegment.from_file(filepath) 
+    timeIncrement = 30 * 1000 # 30 seconds -> 30000 ms
+    totalTime = len(audio) # total time in ms
     audio_chunks = []
-    newAudio = AudioSegment.from_wav(filepath)
+   
     time = 0
-    timeIncrement = 30 * 1000 # (ms)
-    totalTime = newAudio.duration_seconds * 1000 # (ms)
-    while(time != totalTime):
-        audio_chunks.append(newAudio[time:time+timeIncrement])
+    while time < totalTime:
+        audio_chunks.append(audio[time:time+timeIncrement])
         time += timeIncrement
 
     # load audio and pad/trim it to fit 30 seconds for sliding window
     result = ""
-    for chunk in audio_chunks:
-        with open(f"temp{time}.wav", 'wb') as f:
-            f.write()
-        audio = whisper.load_audio(f"temp{time}.wav") # this expects filepath not memory     
+    os.mkdir(temp_dir)
+    for i, chunk in enumerate(audio_chunks):
+        chunk.export(out_f = f"{temp_dir}/time_split{i}{file_extension}", format = file_extension.replace('.', ''))
+        audio = whisper.load_audio(f"{temp_dir}/time_split{i}{file_extension}") # this expects filepath not memory     
         audio = whisper.pad_or_trim(audio)
 
         # make log-Mel spectrogram and move to the same device as the model
@@ -56,11 +63,20 @@ def transcribe(filepath):
         language = max(probs, key=probs.get)
 
         # decode the audio
-        options = whisper.DecodingOptions(task="Translate" if language != "en" else "transcribe")
+        # options = whisper.DecodingOptions(task="Translate" if language != "en" else "transcribe")
+        options = whisper.DecodingOptions()
         decoded = whisper.decode(model, mel, options)
-        result += decoded.text
+        result += f"{decoded.text}"
 
-    return result, language
+    shutil.rmtree(temp_dir)
+    final_result = ""
+    for i, char in enumerate(result):
+        if char.isupper() and result[i-2] != "," and i >= 2:
+            final_result = final_result[:-1]
+            final_result += ". "
+        final_result += char
+
+    return final_result, language
 
 if __name__ == "__main__":
     transcribe()
